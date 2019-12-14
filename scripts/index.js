f42f891c2fb37c93e840f646623cc5179c407bbc
triasmoro <tri_asmoro_wpl@yahoo.com>
2019-12-14 11:03:59 +0700

    feat: add my email when submit contact-form

--- scripts/index.js
+++ scripts/index.js
@@ -153,7 +153,7 @@ $(function() {
     e.preventDefault();
 
       $.ajax({
-          url: "https://formspree.io/mattwilliams85@gmail.com",
+          url: "https://formspree.io/mwkpdjan",
           method: "POST",
           data: { message: $('form').serialize() },
           dataType: "json"
